ce4cfdf25546cce21746a4bdbb959a56d3c8854b
Oğuzhan Aydın <oguzhanaydin56@gmail.com>
2018-04-30 04:53:43 +0300

    Added 3rd party component

---
+++ DatingApp.SPA/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap';
+
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { AlertifyService } from './_services/alertify.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
+],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
+  ],
+  providers: [
+    AuthService,
+    AlertifyService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ DatingApp.SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService, private alertifyService: AlertifyService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(data => {
+      this.alertifyService.success('Logged in successfully');
+    }, error => {
+      this.alertifyService.error(error);
+    });
+  }
+
+  logout() {
+    this.authService.userToken = null;
+    localStorage.removeItem('token');
+    this.alertifyService.warning('Logged out');
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+}
